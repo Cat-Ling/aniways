5e35cf20376b6c4378c75e673794a12c6e138204
Nasrullah <nasrullah01n@gmail.com>
2024-01-27 01:58:09 +0800

    Added hover styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
-import { ArrowLeft, ArrowRight } from 'lucide-react';
+import { ArrowLeft, ArrowRight, Play } from 'lucide-react';
+import { unstable_noStore } from 'next/cache';
 import Link from 'next/link';
 import { parse } from 'node-html-parser';
 
@@ -8,13 +9,10 @@ const Home = async ({
 }: {
   searchParams: { page: string };
 }) => {
+  unstable_noStore();
+
   const res = await fetch(
-    'https://gogotaku.info/recent-release-anime?page=' + (page || 1),
-    {
-      next: {
-        revalidate: 60,
-      },
-    }
+    'https://gogotaku.info/recent-release-anime?page=' + (page || 1)
   ).then(res => res.text());
 
   const recentlyReleased = parse(res)
@@ -50,13 +48,14 @@ const Home = async ({
             size={'icon'}
             variant={'ghost'}
             disabled={Number(page || 1) === 1}
+            asChild
           >
             <Link href={`/?page=${Number(page || 1) - 1}`}>
               <ArrowLeft />
             </Link>
           </Button>
           <p className="text-muted-foreground">{page || 1}</p>
-          <Button size={'icon'} variant={'ghost'}>
+          <Button size={'icon'} variant={'ghost'} asChild>
             <Link href={`/?page=${Number(page || 1) + 1}`}>
               <ArrowRight />
             </Link>
@@ -67,21 +66,29 @@ const Home = async ({
         {recentlyReleased.map(({ name, image, episode, url }, i) => (
           <li
             key={i}
-            className="bg-background border rounded-md border-border p-2"
+            className="bg-background border rounded-md border-border p-2 group"
           >
             <a
               href={`https://anitaku.to/${url}`}
               className="h-full flex flex-col gap-3"
             >
-              <div
-                className="w-full aspect-[450/650] bg-cover rounded-md"
-                style={{
-                  backgroundImage: `url(${image})`,
-                }}
-              />
+              <div className="relative">
+                <div
+                  className="w-full aspect-[450/650] bg-cover rounded-md"
+                  style={{
+                    backgroundImage: `url(${image})`,
+                  }}
+                />
+                <div className="absolute left-0 top-0 w-full h-full bg-muted/60 opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition flex items-center justify-center flex-col">
+                  <Play className="w-8 h-8" />
+                  <p className="text-foreground mt-2 text-lg font-bold">
+                    Watch Now
+                  </p>
+                </div>
+              </div>
               <div className="flex justify-between flex-col flex-1">
                 <p className="text-sm line-clamp-2">{name}</p>
-                <p className="text-sm text-muted-foreground">
+                <p className="text-sm text-muted-foreground mt-1">
                   Episode {episode}
                 </p>
               </div>
