919be823c2954c641732889f215fbd932501aaf7
Nasrullah <nasrullah01n@gmail.com>
2024-01-29 00:28:36 +0800

    Shortened items on pgae

--- src/data-access/anime.ts
+++ src/data-access/anime.ts
@@ -6,8 +6,12 @@ export type Anime = AwaitedReturnType<typeof getRecentlyReleased>[0];
 export const getRecentlyReleased = async (page: number) => {
   unstable_noStore();
 
+  // each page is 60 anime we want 30 per page
+  // so we need to get the page number and divide by 2
+  // if the page is even we want the second 30
+  // if the page is odd we want the first 30
   const res = await fetch(
-    'https://gogotaku.info/recent-release-anime?page=' + page
+    'https://gogotaku.info/recent-release-anime?page=' + Math.ceil(page / 2)
   ).then(res => res.text());
 
   const recentlyReleased = parse(res)
@@ -32,7 +36,8 @@ export const getRecentlyReleased = async (page: number) => {
         episode,
         url: url,
       };
-    });
+    })
+    .splice(page % 2 === 0 ? 30 : 0, 30);
 
   return recentlyReleased;
 };
