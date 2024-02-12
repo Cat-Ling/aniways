16750b33fdc37727fa55aca2e927407d34c3e837
Nasrullah <nasrullah01n@gmail.com>
2024-02-13 00:31:29 +0800

    Changed timeout time to be 5 min for lambda

--- apps/aws-cron/src/index.ts
+++ apps/aws-cron/src/index.ts
@@ -3,6 +3,7 @@ import type { AWS } from '@serverless/typescript';
 export default {
   cron: {
     handler: 'src/handlers/cron.main',
+    timeout: 300,
     events: [
       {
         schedule: 'cron(0 * * * ? *)', // every hour
