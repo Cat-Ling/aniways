4b38aa219414faa68128a3c36130e8539e887959
Nasrullah <nasrullah01n@gmail.com>
2024-03-30 01:23:13 +0800

    Fixed image rendering issues on footer

--- apps/website/app/footer.tsx
+++ apps/website/app/footer.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
 import Link from 'next/link';
+import { Image } from '@aniways/ui/components/ui/aniways-image';
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
