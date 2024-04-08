52c5b587ad8b27acc525b296784150b18c097fe1
Nasrullah <nasrullah01n@gmail.com>
2024-04-08 16:59:23 +0800

    Cleaned up code

--- apps/website/app/(site)/anime/[id]/_metadata/anime-metadata.tsx
+++ apps/website/app/(site)/anime/[id]/_metadata/anime-metadata.tsx
@@ -4,10 +4,9 @@ import { getAnimeDetailsFromMyAnimeList } from '@aniways/myanimelist';
 import { cookies } from 'next/headers';
 import { MetadataProvider } from './metadata-provider';
 import { AnimeMetadataClient } from './anime-metadata-client';
-import Link from 'next/link';
-import Image from 'next/image';
-import { Skeleton } from '@ui/components/ui/skeleton';
-import { Play } from 'lucide-react';
+import { Suspense } from 'react';
+import { AnimeGridLoader } from '../../../anime-grid';
+import { RelatedAnime } from './related-anime';
 
 type AnimeMetadataProps = {
   anime: schema.Anime;
@@ -30,82 +29,15 @@ export const AnimeMetadata = async ({ anime }: AnimeMetadataProps) => {
       .where(orm.eq(schema.anime.id, anime.id));
   }
 
-  if (!details) return null;
-
-  const relatedAnime = (
-    await Promise.all(
-      details.relatedAnime.map(async anime => ({
-        ...anime,
-        id: await db.query.anime
-          .findFirst({
-            where: (fields, actions) => {
-              return actions.eq(fields.malAnimeId, anime.node.id);
-            },
-            columns: {
-              id: true,
-            },
-          })
-          .then(data => data?.id),
-      }))
-    )
-  ).sort((a, b) => {
-    if (a.relation_type === 'side_story') return 1;
-    if (b.relation_type === 'side_story') return -1;
-    return 0;
-  });
+  if (!details || !details.mal_id) return null;
 
   return (
     <MetadataProvider metadata={details}>
       <h3 className="mb-3 mt-6 text-lg font-semibold">Anime Information</h3>
       <AnimeMetadataClient anime={anime} />
-      {details.relatedAnime.length > 0 && (
-        <>
-          <h3 className="mb-3 mt-6 text-lg font-semibold">Related Anime</h3>
-          <section className="grid h-full grid-cols-2 gap-3 md:grid-cols-5">
-            {relatedAnime.map(anime => {
-              const url =
-                anime.id ?
-                  `/anime/${anime.id}`
-                : '/search?query=' + encodeURIComponent(anime.node.title);
-
-              return (
-                <Link
-                  href={url}
-                  key={anime.node.id}
-                  className="bg-background border-border group flex h-full flex-col gap-3 rounded-md border p-2"
-                >
-                  <div className="relative">
-                    <div className="relative aspect-[450/650] w-full overflow-hidden rounded-md">
-                      <Skeleton className="absolute z-0 h-full w-full rounded-md" />
-                      <Image
-                        src={anime.node.main_picture.large}
-                        alt={anime.node.title}
-                        width={450}
-                        height={650}
-                        className="absolute h-full w-full object-cover"
-                      />
-                    </div>
-                    <div className="bg-muted/70 pointer-events-none absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center opacity-0 transition group-hover:pointer-events-auto group-hover:opacity-100">
-                      <Play className="text-primary h-8 w-8" />
-                      <p className="text-foreground mt-2 text-lg font-bold">
-                        Watch Now
-                      </p>
-                    </div>
-                  </div>
-                  <div className="flex flex-1 flex-col justify-between">
-                    <p className="group-hover:text-primary line-clamp-2 text-xs transition md:text-sm">
-                      {anime.node.title}
-                    </p>
-                    <p className="text-muted-foreground mt-1 text-xs md:text-sm">
-                      {anime.relation_type_formatted}
-                    </p>
-                  </div>
-                </Link>
-              );
-            })}
-          </section>
-        </>
-      )}
+      <Suspense fallback={<AnimeGridLoader />}>
+        <RelatedAnime details={details} />
+      </Suspense>
     </MetadataProvider>
   );
 };
---
+++ apps/website/app/(site)/anime/[id]/_metadata/related-anime.tsx
@@ -0,0 +1,90 @@
+import { db } from '@aniways/database';
+import { getAnimeDetailsFromMyAnimeList } from '@aniways/myanimelist';
+import { Image } from '@ui/components/ui/aniways-image';
+import { Skeleton } from '@ui/components/ui/skeleton';
+import { Play } from 'lucide-react';
+import Link from 'next/link';
+import { FC } from 'react';
+
+type RelatedAnimeProps = {
+  details: Awaited<ReturnType<typeof getAnimeDetailsFromMyAnimeList>>;
+};
+
+export const RelatedAnime: FC<RelatedAnimeProps> = async props => {
+  const { details } = props;
+
+  if (!details || !details.relatedAnime) return null;
+
+  const relatedAnime = (
+    await Promise.all(
+      details.relatedAnime.map(async anime => ({
+        ...anime,
+        id: await db.query.anime
+          .findFirst({
+            where: (fields, actions) => {
+              return actions.eq(fields.malAnimeId, anime.node.id);
+            },
+            columns: {
+              id: true,
+            },
+          })
+          .then(data => data?.id),
+      }))
+    )
+  ).sort((a, b) => {
+    if (a.relation_type === 'side_story') return 1;
+    if (b.relation_type === 'side_story') return -1;
+    return 0;
+  });
+
+  if (relatedAnime.length === 0) return null;
+
+  return (
+    <>
+      <h3 className="mb-3 mt-6 text-lg font-semibold">Related Anime</h3>
+      <section className="grid h-full grid-cols-2 gap-3 md:grid-cols-5">
+        {relatedAnime.map(anime => {
+          const url =
+            anime.id ?
+              `/anime/${anime.id}`
+            : '/search?query=' + encodeURIComponent(anime.node.title);
+
+          return (
+            <Link
+              href={url}
+              key={anime.node.id}
+              className="bg-background border-border group flex h-full flex-col gap-3 rounded-md border p-2"
+            >
+              <div className="relative">
+                <div className="relative aspect-[450/650] w-full overflow-hidden rounded-md">
+                  <Skeleton className="absolute z-0 h-full w-full rounded-md" />
+                  <Image
+                    src={anime.node.main_picture.large}
+                    alt={anime.node.title}
+                    width={450}
+                    height={650}
+                    className="absolute h-full w-full object-cover"
+                  />
+                </div>
+                <div className="bg-muted/70 pointer-events-none absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center opacity-0 transition group-hover:pointer-events-auto group-hover:opacity-100">
+                  <Play className="text-primary h-8 w-8" />
+                  <p className="text-foreground mt-2 text-lg font-bold">
+                    Watch Now
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-1 flex-col justify-between">
+                <p className="group-hover:text-primary line-clamp-2 text-xs transition md:text-sm">
+                  {anime.node.title}
+                </p>
+                <p className="text-muted-foreground mt-1 text-xs md:text-sm">
+                  {anime.relation_type_formatted}
+                </p>
+              </div>
+            </Link>
+          );
+        })}
+      </section>
+    </>
+  );
+};
