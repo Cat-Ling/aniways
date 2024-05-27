73469220f937d6ba7bbc3795e1beadd8046122ff
Nasrullah <nasrullah01n@gmail.com>
2024-05-27 22:19:07 +0800

    Added some ad blocking

--- apps/video/src/handlers/video.ts
+++ apps/video/src/handlers/video.ts
@@ -1,13 +1,98 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { EpisodeService } from '@aniways/data';
+import { z } from 'zod';
 
-export const getVideo: APIGatewayProxyHandler = async () => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'html/text; charset=utf-8',
-    },
-    body: JSON.stringify({
-      message: 'Hello from the Video service',
-    }),
+const PathSchema = z.tuple([
+  z.string().cuid2(),
+  z.coerce.number().transform(value => value.toString()),
+]);
+
+export const getVideo: APIGatewayProxyHandler = async ({ path }) => {
+  console.log('path', path);
+
+  try {
+    const [videoId, episode] = PathSchema.parse(path.split('/').slice(1));
+
+    const service = new EpisodeService();
+
+    const url = await service.getEpisodeUrl(videoId, episode);
+
+    let html = await fetch(url).then(response => response.text());
+
+    // add meta base tag
+    const baseTag = `<base href="${new URL(url).origin}">`;
+    const headEnd = html.indexOf('</head>');
+    html = html.slice(0, headEnd) + baseTag + html.slice(headEnd);
+
+    // remove scripts which contains ads
+    const adsScript = `
+      <script>
+        Array.from(document.querySelectorAll('script')).forEach(script => {
+          if (script.src.includes('ads')) {
+            script.remove();
+          }
+        })
+      </script>`;
+    const bodyStart = html.indexOf('<body');
+    html = html.slice(0, bodyStart) + adsScript + html.slice(bodyStart);
+
+    const script = `
+      <script>
+        const removeUnusedLinks = () => {
+          const link = document.querySelector('.linkserver[data-status="1"]');
+          document.querySelector('.linkserver[data-status="0"]')?.remove();
+          link.click();
+        }
+
+        const observer = new MutationObserver(() => {
+          const iframes = Array.from(document.querySelectorAll('iframe'));
+          for (const iframe of iframes) {
+            if (iframe.parentElement.id === 'load-iframe') {
+              continue;
+            }
+            iframe.remove();
+          }
+        })
+
+        window.addEventListener('load', () => {
+          removeUnusedLinks();
+          observer.observe(document.querySelector('html'), { childList: true, subtree: true });
+        });
+      </script>`;
+
+    const bodyEnd = html.indexOf('</body>');
+    html = html.slice(0, bodyEnd) + script + html.slice(bodyEnd);
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html; charset=utf-8',
+      },
+      body: html,
+    };
+  } catch (error) {
+    console.error('error', error);
+
+    if (error instanceof z.ZodError) {
+      return {
+        statusCode: 404,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          message: 'Not Found',
+        }),
+      };
+    }
+
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        message: 'Internal Server Error',
+      }),
+    };
   }
-}
+};
--- apps/video/src/index.ts
+++ apps/video/src/index.ts
@@ -1,4 +1,15 @@
 import type { AWS } from '@serverless/typescript';
 
 export default {
+  video: {
+    handler: 'src/handlers/video.getVideo',
+    events: [
+      {
+        http: {
+          method: 'get',
+          path: '/{any+}',
+        }
+      }
+    ]
+  }
 } satisfies AWS['functions'];
