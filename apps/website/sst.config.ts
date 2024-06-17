2b630c4a64f943dbe29e09cbbabb393b7d2dbede
Nasrullah <nasrullah01n@gmail.com>
2024-06-18 01:48:39 +0800

    Try deploying to aws

--- apps/website/sst.config.ts
+++ apps/website/sst.config.ts
@@ -1,16 +1,16 @@
-import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { SSTConfig } from 'sst';
+import { NextjsSite } from 'sst/constructs';
 
 export default {
   config(_input) {
     return {
-      name: "website",
-      region: "us-east-1",
+      name: 'aniways-website',
+      region: 'ap-southeast-1',
     };
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, 'site');
 
       stack.addOutputs({
         SiteUrl: site.url,
