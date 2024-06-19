776c08cc908507e6e939ab2bdf3011e364bd6051
Nasrullah <nasrullah01n@gmail.com>
2024-06-19 20:34:39 +0800

    Move to custom domain

--- apps/website/sst.config.ts
+++ apps/website/sst.config.ts
@@ -1,6 +1,7 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
 import { SSTConfig } from 'sst';
 import { NextjsSite } from 'sst/constructs';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 
 export default {
   config(_input) {
@@ -12,6 +13,17 @@ export default {
   stacks(app) {
     app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, 'site', {
+        customDomain: {
+          domainName: 'aniways.xyz',
+          isExternalDomain: true,
+          cdk: {
+            certificate: Certificate.fromCertificateArn(
+              stack,
+              'Certificate',
+              process.env.AWS_CERT_ARN!
+            ),
+          },
+        },
         environment: {
           DATABASE_URL: process.env.DATABASE_URL!,
           MAL_CLIENT_ID: process.env.MAL_CLIENT_ID!,
