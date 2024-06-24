d5a1057c645298ee72f1cd232a7641a085b88904
Nasrullah <nasrullah01n@gmail.com>
2024-06-24 21:54:01 +0800

    Removed custom url for healthcheck api

--- apps/healthcheck/sst.config.ts
+++ apps/healthcheck/sst.config.ts
@@ -9,10 +9,6 @@ const jiti = createJiti(fileURLToPath(import.meta.url));
 jiti("@aniways/db/env");
 jiti("@aniways/myanimelist/env");
 
-if (!process.env.AWS_CERT_ARN_AP_SOUTHEAST_1) {
-  throw new Error("AWS_CERT_ARN_AP_SOUTHEAST_1 is required");
-}
-
 if (!process.env.AWS_CERT_ARN_US_EAST_1) {
   throw new Error("AWS_CERT_ARN_US_EAST_1 is required");
 }
@@ -28,12 +24,6 @@ export default {
       Tags.of(stack).add("Meta", `${app.stage}-${app.region}`);
       Tags.of(stack).add("Purpose", "Health Check");
 
-      const certificateAPSE = Certificate.fromCertificateArn(
-        stack,
-        "ap-southeast-1-certificate",
-        process.env.AWS_CERT_ARN_AP_SOUTHEAST_1!,
-      );
-
       const api = new Api(stack, "healthcheck-api", {
         routes: {
           "GET /": "api/check-service.handler",
@@ -45,13 +35,6 @@ export default {
               DATABASE_URL: process.env.DATABASE_URL!,
               MAL_CLIENT_ID: process.env.MAL_CLIENT_ID!,
             },
-          },
-        },
-        customDomain: {
-          isExternalDomain: true,
-          domainName: "api.healthcheck.aniways.xyz",
-          cdk: {
-            certificate: certificateAPSE,
           },
         },
       });
@@ -73,14 +56,13 @@ export default {
           },
         },
         environment: {
-          VITE_APP_API_URL: api.customDomainUrl!,
+          VITE_APP_API_URL: api.url!,
         },
       });
 
       stack.addOutputs({
         ApiEndpoint: api.url,
-        ApiDomain: api.customDomainUrl,
-        WebsiteUrl: website.url,
+        WebsiteDistrubution: website.url,
         WebsiteDomain: website.customDomainUrl,
       });
     });
