6007563b17746346fad45d767bfbdd4d8e8b8608
Nasrullah <nasrullah01n@gmail.com>
2024-07-05 01:42:54 +0800

    Fix multiple db clients in dev mode issue

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,4 @@
 /* eslint-disable no-restricted-properties */
-import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import * as orm from "drizzle-orm";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
@@ -8,20 +7,22 @@ import { env } from "../env";
 import * as schema from "./schema";
 
 const globalForDrizzle = globalThis as unknown as {
-  db?: PostgresJsDatabase<typeof schema>;
+  client?: postgres.Sql;
 };
 
-const client = postgres(env.DATABASE_URL, { prepare: false });
-
-const db =
-  globalForDrizzle.db ??
-  drizzle(client, {
-    schema,
+const client =
+  globalForDrizzle.client ??
+  postgres(env.DATABASE_URL, {
+    prepare: false,
   });
 
 if (process.env.NODE_ENV === "development") {
-  globalForDrizzle.db = db;
+  globalForDrizzle.client = client;
 }
+
+const db = drizzle(client, {
+  schema,
+});
 
 export { createId } from "@paralleldrive/cuid2";
 export { client, db, orm, schema };
