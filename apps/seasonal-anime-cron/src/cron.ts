f1ae5325b611cd24474213889daca8e7a4083cbe
Nasrullah <nasrullah01n@gmail.com>
2024-07-05 15:53:33 +0800

    Added logging of data to seasonal cron

--- apps/seasonal-anime-cron/src/cron.ts
+++ apps/seasonal-anime-cron/src/cron.ts
@@ -3,6 +3,9 @@ import { api } from "./trpc";
 export const syncSeasonalAnime = async () => {
   const seasonalAnimes = await api.seasonalAnime.getCurrentSeasonalAnimes();
 
+  console.log("Fetched seasonal anime.");
+  console.log(JSON.stringify(seasonalAnimes, null, 2));
+
   await api.seasonalAnime.saveSeasonalAnimes(
     seasonalAnimes.map((anime, i) => ({
       malId: anime.mal_id,
