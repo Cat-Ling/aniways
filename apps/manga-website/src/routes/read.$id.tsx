d46d265853bc921926a639cbd5dbd131976318fa
Nasrullah <nasrullah01n@gmail.com>
2024-07-26 12:37:27 +0800

    Update manga images

--- apps/manga-website/src/routes/read.$id.tsx
+++ apps/manga-website/src/routes/read.$id.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
+
+import { cn } from "@aniways/ui";
 
 import { MainLayout } from "../components/layout";
 import { api } from "../trpc";
@@ -25,16 +28,29 @@ function ReadMangaPage() {
     <MainLayout>
       <h1>Read Manga Page</h1>
       <p>Reading manga with ID: {params.id}</p>
-      {chapter.data?.images.map(image => (
-        <img
-          key={image.url}
-          src={`/images/${image.url}`}
-          alt={image.alt}
-          className="w-full"
-          loading="lazy"
-        />
-      ))}
+      <div className="mx-auto max-w-lg">
+        {chapter.data?.images.map(image => (
+          <MangaImage key={image.url} src={image.url} alt={image.alt} />
+        ))}
+      </div>
       <pre>{JSON.stringify(chapter.data, null, 2)}</pre>
     </MainLayout>
+  );
+}
+
+function MangaImage(props: { src?: string; alt?: string }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <img
+      src={`/images/${props.src}`}
+      alt={props.alt}
+      className={cn(
+        "w-full object-contain object-center",
+        isLoading && "h-[calc(100vw-64px*(2/3))] lg:h-[calc(32rem*(2/3))]"
+      )}
+      loading="lazy"
+      onLoad={() => setIsLoading(false)}
+    />
   );
 }
