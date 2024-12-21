8befb5e77db2fc504189b0881799fe3a373b6730
Nasrullah <nasrullah01n@gmail.com>
2024-12-21 22:58:15 +0800

    Added env check

--- src/env.js
+++ src/env.js
@@ -4,12 +4,17 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    MAL_CLIENT_ID: z.string(),
+    MAL_CLIENT_SECRET: z.string(),
+    MAL_SECRET_KEY: z.string().base64().length(44),
+    MAL_REQUEST_DEBUG:
+      process.env.NODE_ENV === "development"
+        ? z.literal("true").optional()
+        : z.coerce.boolean().optional(),
+    DATABASE_URL: z.string().url(),
   },
-  client: {
-  },
-  runtimeEnv: {
-    NODE_ENV: process.env.NODE_ENV,
-  },
+  client: {},
+  experimental__runtimeEnv: {},
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,
 });
