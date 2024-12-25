02293cf1a216a916d07d5dbf971282af280ef663
Nasrullah <nasrullah01n@gmail.com>
2024-12-25 14:47:06 +0800

    Updated loading ui

--- src/app/(site)/(home)/layout.tsx
+++ src/app/(site)/(home)/layout.tsx
@@ -12,14 +12,29 @@ type HomeLayoutProps = {
 const HomeLayout = ({ children }: HomeLayoutProps) => {
   return (
     <>
-      <Suspense fallback={<Skeleton className="mb-6 h-[450px] w-full" />}>
+      <Suspense
+        fallback={
+          <div className="mb-6 flex w-full flex-col-reverse gap-3 md:grid md:grid-cols-5 md:gap-6">
+            <div className="col-span-2 flex select-none flex-col justify-center">
+              <Skeleton className="mb-2 line-clamp-3 h-6 w-1/2 text-2xl font-bold md:mb-5 md:h-12 md:text-5xl" />
+              <Skeleton className="mb-3 flex h-10 w-3/4 gap-2" />
+              <Skeleton className="mb-2 line-clamp-3 h-16 w-full text-sm text-muted-foreground md:mb-5" />
+              <Skeleton className="flex h-10 w-36 items-center gap-2" />
+            </div>
+            <Skeleton className="relative col-span-3 aspect-video w-full overflow-hidden rounded-md p-3" />
+          </div>
+        }
+      >
         <AnimeCarousel />
       </Suspense>
       <Suspense
         fallback={
-          <div className="mb-6">
-            <AnimeGridLoader />
-          </div>
+          <>
+            <Skeleton className="mb-2 h-7 w-60 font-bold md:mb-5 md:h-8" />
+            <div className="mb-6">
+              <AnimeGridLoader />
+            </div>
+          </>
         }
       >
         <ContinueWatching />
--- src/app/(site)/(home)/loading.tsx
+++ src/app/(site)/(home)/loading.tsx
@@ -1,7 +1,15 @@
 import { AnimeGridLoader } from "@/components/layouts/anime-grid-loader";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const HomePageLoading = () => {
-  return <AnimeGridLoader />;
+  return (
+    <>
+      <Skeleton className="mb-2 h-7 w-60 font-bold md:mb-5 md:h-8" />
+      <div className="mb-6">
+        <AnimeGridLoader />
+      </div>
+    </>
+  );
 };
 
 export default HomePageLoading;
