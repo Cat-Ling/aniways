4a9e1d99f6b0b1a36c246231eb5eebcdef2c338f
Nasrullah <83153784+Coeeter@users.noreply.github.com>
2024-12-29 09:12:32 +0800

    Fix static rendering of some pages

--- src/app/(site)/watching/page.tsx
+++ src/app/(site)/watching/page.tsx
@@ -2,6 +2,8 @@ import { ContinueWatching } from "@/components/anime/continue-watching";
 import { api } from "@/trpc/server";
 import { notFound } from "next/navigation";
 
+export const dynamic = 'force-dynamic';
+
 const CurrentlyWatchingPage = async () => {
   const initalData = await api.mal
     .getContinueWatching({ page: 1 })
