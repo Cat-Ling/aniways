7ecd8fac1eb7ac4eb4d51462fd8b2d36d5521912
Nasrullah <nasrullah01n@gmail.com>
2025-01-01 13:32:34 +0800

    Added mappings table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,22 +1,35 @@
 import type { InferSelectModel } from "drizzle-orm";
-import { integer, jsonb, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  index,
+  integer,
+  jsonb,
+  pgTable,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
 
 import { cuid2 } from "drizzle-cuid2/postgres";
 
-export const seasonalAnime = pgTable("seasonal_anime", {
-  id: cuid2("id").primaryKey().defaultRandom(),
-  malId: integer("mal_id").notNull(),
-  title: text().notNull(),
-  rating: text(),
-  type: text(),
-  episodes: integer(),
-  synopsis: text(),
-  imageUrl: text("image_url").notNull(),
-  order: integer().notNull(),
-  status: text(),
-});
+export const mappings = pgTable(
+  "mapping",
+  {
+    hiAnimeId: text().primaryKey(),
+    malId: integer(),
+    anilistId: integer(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at")
+      .notNull()
+      .$onUpdateFn(() => new Date()),
+  },
+  (t) => [
+    {
+      malIdx: index("mal_idx").on(t.malId),
+      anilistIdx: index("anilist_idx").on(t.anilistId),
+    },
+  ],
+);
 
-export type SeasonalAnime = InferSelectModel<typeof seasonalAnime>;
+export type Mappings = InferSelectModel<typeof mappings>;
 
 export interface Settings {
   autoPlay: boolean;
@@ -26,7 +39,7 @@ export interface Settings {
 }
 
 export const users = pgTable("users", {
-  id: cuid2("id").primaryKey().defaultRandom(),
+  id: cuid2().primaryKey().defaultRandom(),
   malId: integer("mal_id").notNull().unique(),
   username: text().notNull().unique(),
   picture: text(),
