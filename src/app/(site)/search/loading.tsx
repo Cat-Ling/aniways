14c051555734cda05124695d5a1d99f25bd3b154
Nasrullah <nasrullah01n@gmail.com>
2025-01-25 17:31:12 +0800

    Added better search loading page

--- src/app/(site)/search/loading.tsx
+++ src/app/(site)/search/loading.tsx
@@ -1,7 +1,18 @@
+import { FilterForm } from "@/components/anime/search/filter-form";
 import { AnimeGridLoader } from "@/components/layouts/anime-grid-loader";
 
 const SearchLoading = () => {
-  return <AnimeGridLoader />;
+  return (
+    <>
+      <FilterForm />
+      <div className="mb-6 flex w-full flex-col justify-between gap-3 md:mb-5 md:flex-row md:items-center">
+        <div>
+          <h1 className="text-2xl font-bold">Search</h1>
+        </div>
+      </div>
+      <AnimeGridLoader />
+    </>
+  );
 };
 
 export default SearchLoading;
