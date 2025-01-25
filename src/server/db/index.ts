9d0f0bcccbf2db1ee6ec5eaaf793ffb455509f7c
Nasrullah <nasrullah01n@gmail.com>
2025-01-26 02:07:28 +0800

    Less scuffed way of running migrations in web service

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,5 @@
 import * as orm from "drizzle-orm";
 import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import config from "drizzle.config";
 import postgres from "postgres";
 
 import { env } from "@/env";
@@ -24,31 +22,5 @@ if (process.env.NODE_ENV === "development") {
 const db = drizzle(client, {
   schema,
 });
-
-async function migrateDB() {
-  let isError = false;
-  let count = 50;
-
-  do {
-    await migrate(db, {
-      migrationsFolder: config.out ?? "drizzle",
-    })
-      .then(() => {
-        isError = false;
-      })
-      .catch(async (error) => {
-        console.error(error);
-        isError = true;
-        await new Promise((resolve) => setTimeout(resolve, 5000));
-      });
-  } while (isError && count-- > 0);
-}
-
-if (
-  process.env.NODE_ENV === "production" &&
-  process.env.npm_lifecycle_event !== "build"
-) {
-  await migrateDB();
-}
 
 export { client, db, orm, schema };
