44a674ca91b1d609fdb1a2ba74aa9c27c88ba38d
Nasrullah <nasrullah01n@gmail.com>
2025-01-26 14:35:34 +0800

    Remove pino logging

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,13 +3,4 @@ export async function register() {
     const { migrateDB } = await import("@/server/db/migrate");
     await migrateDB();
   }
-
-  if (
-    process.env.NEXT_RUNTIME === "nodejs" &&
-    process.env.NODE_ENV === "production"
-  ) {
-    await import("pino");
-    // @ts-expect-error: This is a dynamic import
-    await import("next-logger");
-  }
 }
