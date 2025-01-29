de940f95b7e7d34b2e35fa298ca48738bb0929a3
Nasrullah <nasrullah01n@gmail.com>
2025-01-30 00:27:29 +0800

    Fix userexeption

--- api/src/main/kotlin/features/user/exceptions/UserNotFoundException.kt
+++ api/src/main/kotlin/features/user/exceptions/UserNotFoundException.kt
@@ -1,5 +1,3 @@
 package xyz.aniways.features.user.exceptions
 
-class UserNotFoundException(id: String): Exception(
-    message = "User with id $id not found"
-)
+class UserNotFoundException(id: String): Exception("User with id $id not found")
