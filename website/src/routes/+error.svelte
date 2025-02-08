ef2be23552d252cde43bf130ab25d9b6c5c6afad
Nasrullah <nasrullah01n@gmail.com>
2025-02-08 21:13:26 +0800

    Updated size of image

--- website/src/routes/+error.svelte
+++ website/src/routes/+error.svelte
@@ -7,24 +7,20 @@
 	let is404 = $derived(page.status === 404);
 </script>
 
-<div class="mx-auto mt-32 flex w-1/2 flex-col gap-4">
-	<enhanced:img
-		src={sorry}
-		alt="Sorry, this page does not exist"
-		class="mx-auto w-full max-w-screen-md"
-	/>
+<div class="mt-32 flex w-full max-w-screen-md flex-col gap-4 p-3 md:mx-auto md:p-8">
+	<enhanced:img src={sorry} alt="Sorry, this page does not exist" class="mx-auto w-full" />
 	{#if is404}
 		<h1 class="text-center font-sora text-2xl">Sorry, this page does not exist</h1>
-		<p class="text-center">
+		<p class="text-center text-muted-foreground">
 			The page you are looking for does not exist. Please check the URL and try again.
 		</p>
 	{:else}
 		<h1 class="text-center font-sora text-2xl">Sorry, something went wrong</h1>
-		<p class="text-center">
+		<p class="text-center text-muted-foreground">
 			An error occurred while trying to load this page. Please try again later.
 		</p>
 	{/if}
-	<div class="mx-auto flex w-fit justify-center gap-2">
+	<div class="mx-auto flex w-fit flex-col justify-center gap-2 md:flex-row">
 		<Button variant="secondary" onclick={() => history.back()} class="gap-1">
 			<ChevronLeft class="size-5" />
 			Go back
