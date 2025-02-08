f2865a5d1b3284b025f2cf4d210e3b463662af4a
Nasrullah <nasrullah01n@gmail.com>
2025-02-08 17:39:18 +0800

    Remove server-side rendering for login page

--- website/src/routes/auth/login/+page.server.ts
+++ website/src/routes/auth/login/+page.server.ts
@@ -2,8 +2,6 @@ import { getLoginUrl } from '$lib/api/auth';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const ssr = false;
-
 export const load: PageServerLoad = async ({ url }) => {
 	const login = await getLoginUrl(url.searchParams.get('redirect') ?? undefined);
 
