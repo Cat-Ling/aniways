783679b6e2833ccfbfc6d5c48d8885fb56de4d3b
Nasrullah <nasrullah01n@gmail.com>
2025-02-16 11:06:22 +0800

    Remove browser check from handleFetch to streamline request handling

--- website/src/hooks.server.ts
+++ website/src/hooks.server.ts
@@ -1,14 +1,9 @@
-import { browser } from '$app/environment';
 import { PUBLIC_API_URL } from '$env/static/public';
 import type { HandleFetch } from '@sveltejs/kit';
 
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
 	if (request.url.startsWith(PUBLIC_API_URL)) {
 		request.headers.set('cookie', event.request.headers.get('cookie') || '');
-	}
-
-	if (!browser) {
-		return fetch(request);
 	}
 
 	return fetch(request, {
