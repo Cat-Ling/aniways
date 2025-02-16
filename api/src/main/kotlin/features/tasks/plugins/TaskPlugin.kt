2b49ab81d39aebfd28043619bd87ce643b4cf6d0
Nasrullah <83153784+Coeeter@users.noreply.github.com>
2025-02-16 11:39:57 +0800

    Remove task routes

--- api/src/main/kotlin/features/tasks/plugins/TaskPlugin.kt
+++ api/src/main/kotlin/features/tasks/plugins/TaskPlugin.kt
@@ -17,42 +17,6 @@ val TaskSchedulerPlugin = createApplicationPlugin(
     val tasksByFrequency = tasks.groupBy { it.frequency }
     val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
-    onCall { call ->
-        val uri = call.request.uri
-
-        val getAllTasksRegex = "/tasks".toRegex()
-        val runTaskRegex = "/tasks/([a-zA-Z0-9]+)".toRegex()
-
-        if (!this@createApplicationPlugin.application.isDev) {
-            return@onCall
-        }
-
-        when {
-            getAllTasksRegex.matches(call.request.uri) -> {
-                val response = tasks
-                    .filter { it.frequency !is TaskScheduler.Frequency.OnStartUp }
-                    .map { task -> task.toDto() }
-
-                call.respond(response)
-            }
-
-            runTaskRegex.matches(call.request.uri) -> {
-                val taskName = runTaskRegex.find(uri)!!.groupValues[1]
-
-                val task = tasks.find {
-                    it.name == taskName && it.frequency !is TaskScheduler.Frequency.OnStartUp
-                } ?: return@onCall call.respond(HttpStatusCode.NotFound)
-
-                scope.launch {
-                    scheduler.execute(task)
-                }
-
-                call.respondText("Task ${task.name} started")
-            }
-
-        }
-    }
-
     on(MonitoringEvent(ApplicationStarted)) {
         val startUpTasks = tasksByFrequency[TaskScheduler.Frequency.OnStartUp] ?: emptyList()
 
