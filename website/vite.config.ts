5c3d263d00b9a56c6e896b281bd2ae017ad4de30
Nasrullah <nasrullah01n@gmail.com>
2025-02-25 00:22:36 +0800

    Removed some plugins

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,29 +1,8 @@
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { visualizer } from 'rollup-plugin-visualizer';
 import lucidePreprocess from 'vite-plugin-lucide-preprocess';
 
 export default defineConfig({
-	plugins: [
-		enhancedImages(),
-		sveltekit(),
-		visualizer({
-			emitFile: true,
-			filename: 'stats.html'
-		}),
-		lucidePreprocess(),
-		{
-			name: 'vite-plugin-patch-ssr-noexternal',
-			enforce: 'post',
-			config(cfg) {
-				if (!cfg.ssr?.noExternal) return;
-				cfg.ssr.noExternal = cfg.ssr.noExternal.map((x) =>
-					typeof x !== 'string' || x.includes('*') || x.endsWith('/**')
-						? x
-						: `${x}${!x.endsWith('/') ? '/' : ''}**`
-				);
-			}
-		}
-	]
+	plugins: [enhancedImages(), sveltekit(), lucidePreprocess()]
 });
