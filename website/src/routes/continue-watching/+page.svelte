50e6db5f24bf2ecb052946781a5f03217c3adec7
Coeeter <nasrullah01n@gmail.com>
2025-03-23 19:11:36 +0800

    Remove unnecessary class from AnimeGrid in continue watching and plan to watch pages

--- website/src/routes/continue-watching/+page.svelte
+++ website/src/routes/continue-watching/+page.svelte
@@ -8,7 +8,6 @@
 <div class="m-3 mt-20 md:m-8 md:mt-20">
   <h1 class="font-sora text-2xl font-bold">Continue Watching</h1>
   <AnimeGrid
-    class="md:last:[&_a]:hidden"
     pageInfo={data.continueWatching.pageInfo}
     buildUrl={(anime) => `/anime/${anime.id}/watch?episode=${anime.lastEpisode}`}
     buildSubtitle={(anime) => `Episode ${anime.lastEpisode}`}
--- website/src/routes/plan-to-watch/+page.svelte
+++ website/src/routes/plan-to-watch/+page.svelte
@@ -8,7 +8,6 @@
 <div class="m-3 mt-20 md:m-8 md:mt-20">
   <h1 class="font-sora text-2xl font-bold">Plan To Watch</h1>
   <AnimeGrid
-    class="md:last:[&_a]:hidden"
     pageInfo={data.planToWatch.pageInfo}
     buildUrl={(anime) => `/anime/${anime.id}/watch?episode=${anime.lastEpisode}`}
     buildSubtitle={(anime) => `Episode ${anime.lastEpisode}`}
